5b402fa351e10f25fa94a2c269015a04fc51829e
unknown <rafiearmansyah@gmail.com>
2026-07-20 21:41:29 +0700

    nambahin js

--- belajar javascript/script.js
+++ belajar javascript/script.js
@@ -1,1 +1,6 @@
 console.log("Mesin JavaScript berhasil menyala, Bos! Siap menerima logika!");
+
+const teksJudul = document.getElementByid("judul");
+const teksDeskripsi = document.getElementByid("deskripsi");
+const tombolSakti = document.getElementByid("tombol-sakti");
+
