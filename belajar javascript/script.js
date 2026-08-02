5ca2243844b095a22432de4418342c775ea2474f
unknown <rafiearmansyah@gmail.com>
2026-08-02 14:53:43 +0700

    fix bug lagi

--- belajar javascript/script.js
+++ belajar javascript/script.js
@@ -4,46 +4,32 @@ const teksJudul = document.getElementById("judul");
 const teksDeskripsi = document.getElementById("deskripsi");
 const tombolSakti = document.getElementById("tombol-aksi");
 
-// ==========================================
-// 1. MEMBUAT MEMORI (VARIABEL STATUS)
-// Kita pakai 'let' (bukan 'const') karena isi memori ini bakal kita ubah-ubah.
-// false = mesin mati, true = mesin nyala.
-// ==========================================
-let mesinNyala = false; 
+let mesinNyala =false;
 
 tombolSakti.addEventListener("click", function() {
+    
 
-    // ==========================================
-    // 2. MASUK KE LOGIKA SAKLAR (IF / ELSE)
-    // Tanda '===' artinya JavaScript lagi nanya: "Apakah nilainya bener-bener sama dengan...?"
-    // ==========================================
-
-    if (mesinNyala === false) {
-        
-        // IF: JIKA MESIN SEDANG MATI, MAKA NYALAKAN!
-        teksJudul.innerText = "Sistem Berhasil Aktif! 🔥";
-        teksDeskripsi.innerText = "Mesin sekarang dalam keadaan MENYALA.";
+    if (mesinNyala == false) {
+        teksJudul.innerText = "sistem Berhasil Aktif🔥";
+        teksDeskripsi.innerText = "Mesin Sekarang dalam ke adaan Menyala";
         teksDeskripsi.style.color = "#00ffcc";
 
-        tombolSakti.innerText = "Matikan Mesin 🛑";
-        tombolSakti.style.backgroundColor = "#e63946"; // Tombol jadi merah
-        
-        // PENTING: Update memorinya biar tombol tau sekarang mesin udah nyala!
-        mesinNyala = true; 
+        tombolSakti.innerText = "Matikan Mesin🔴";
+        tombolSakti.style.backgroundColor = "#e63946";
+
+        mesinNyala = true;
 
     } else {
-        
-        // ELSE: SEBALIKNYA (JIKA MESIN SUDAH NYALA), MAKA MATIKAN!
         teksJudul.innerText = "Sistem Belum Aktif";
         teksDeskripsi.innerText = "Menunggu perintah pertama dari tombol...";
-        teksDeskripsi.style.color = "white"; // Balikin teks deskripsi ke warna putih
+        teksDeskripsi.style.color = "white";
 
         tombolSakti.innerText = "Nyalakan Mesin!! 🚀";
-        tombolSakti.style.backgroundColor = "#00ffcc"; // Balikin tombol ke warna cyan
+        tombolSakti.style.backgroundColor = "#00ffcc"; 
         
-        // PENTING: Update memorinya biar tombol tau sekarang mesin mati lagi!
+        
         mesinNyala = false;
-        
     }
-    
-});
+
+
+})
