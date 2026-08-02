b0ea7ddca555fa06c175dc9afc5a1870494c3e88
unknown <rafiearmansyah@gmail.com>
2026-08-02 14:16:49 +0700

    lanjutin codingan ngebuat javascript

--- belajar javascript/script.js
+++ belajar javascript/script.js
@@ -1,6 +1,22 @@
 console.log("Mesin JavaScript berhasil menyala, Bos! Siap menerima logika!");
 
-const teksJudul = document.getElementByid("judul");
-const teksDeskripsi = document.getElementByid("deskripsi");
-const tombolSakti = document.getElementByid("tombol-sakti");
+const teksJudul = document.getElementById("judul");
+const teksDeskripsi = document.getElementById("deskripsi");
 
+
+const tombolSakti = document.getElementById("tombol-aksi"); 
+
+tombolSakti.addEventListener("click", function() {
+
+    teksJudul.innerText = "Selamat datang di dunia JavaScript, Bos!";
+    teksDeskripsi.innerText = "Selamat Datang di lab Java script";
+    
+    teksDeskripsi.style.color = "#00ffcc";
+
+    tombolSakti.innerText = "Mesin Menyala!!";
+    tombolSakti.style.backgroundColor = "#e63946";
+    
+    
+    tombolSakti.style.cursor = "not-allowed"; 
+    
+});
